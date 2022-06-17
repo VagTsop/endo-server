ac031a6eebb580825e6d928863358ea403471489
VagTsop <vatsop52@gmail.com>
2022-06-17 21:57:38 +0300

    create profiles

--- src/main/java/org/endofusion/endoserver/EndoServerApplication.java
+++ src/main/java/org/endofusion/endoserver/EndoServerApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class EndoServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EndoServerApplication.class, args);
 	}
-
 }
--- src/main/java/org/endofusion/endoserver/config/DBConfig.java
+++ src/main/java/org/endofusion/endoserver/config/DBConfig.java
@@ -2,30 +2,57 @@ package org.endofusion.endoserver.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DBConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
 
+    @Profile("dev")
     @Bean
-    public DataSource getDataSource() {
+    public DataSource getDevDataSource() {
         HikariConfig config = new HikariConfig();
         HikariDataSource ds;
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/endo?useSSL=false&useUnicode=yes&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&serverTimezone=UTC");
-        config.setUsername("root");
-        config.setPassword("root");
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
         ds = new HikariDataSource(config);
         return ds;
     }
-
+    @Profile("dev")
     @Bean
-    public NamedParameterJdbcTemplate namedParamJdbcTemplate() {
+    public NamedParameterJdbcTemplate namedParamDevJdbcTemplate() {
         NamedParameterJdbcTemplate namedParamJdbcTemplate =
-                new NamedParameterJdbcTemplate(getDataSource());
+                new NamedParameterJdbcTemplate(getDevDataSource());
+        return namedParamJdbcTemplate;
+    }
+    @Profile("prod")
+    @Bean
+    public DataSource getProdDataSource() {
+        HikariConfig config = new HikariConfig();
+        HikariDataSource ds;
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        ds = new HikariDataSource(config);
+        return ds;
+    }
+    @Profile("prod")
+    @Bean
+    public NamedParameterJdbcTemplate namedParamProdJdbcTemplate() {
+        NamedParameterJdbcTemplate namedParamJdbcTemplate =
+                new NamedParameterJdbcTemplate(getProdDataSource());
         return namedParamJdbcTemplate;
     }
 }
