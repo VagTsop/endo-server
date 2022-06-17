95ee4f722064d00adcca92ff207a863c13865aae
VagTsop <vatsop52@gmail.com>
2022-06-17 18:07:41 +0300

    test

--- src/main/java/org/endofusion/endoserver/EndoServerApplication.java
+++ src/main/java/org/endofusion/endoserver/EndoServerApplication.java
@@ -2,8 +2,10 @@ package org.endofusion.endoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class EndoServerApplication {
 
 	public static void main(String[] args) {
