111e7cf09b2b02a6917067c068c8fbcca42e6d88
VagTsop <vatsop52@gmail.com>
2022-06-18 14:54:07 +0300

    s

--- src/main/java/org/endofusion/endoserver/config/DBConfig.java
+++ src/main/java/org/endofusion/endoserver/config/DBConfig.java
@@ -5,9 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
 import javax.sql.DataSource;
 
 @Configuration
