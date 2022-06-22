5ccc80fa9b2870e6491ae57a0d4b4c8bcd27244f
VagTsop <vatsop52@gmail.com>
2022-06-22 18:35:58 +0300

    test backend pipeline

--- src/main/java/org/endofusion/endoserver/repository/InstrumentSeriesRepositoryImpl.java
+++ src/main/java/org/endofusion/endoserver/repository/InstrumentSeriesRepositoryImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository("InstrumentSeriesRepository")
-public class InstrumentSeriesRepositoryImpl implements InstrumentSeriesRepository {//
+public class InstrumentSeriesRepositoryImpl implements InstrumentSeriesRepository {
 
     @Autowired
     protected NamedParameterJdbcTemplate namedParameterJdbcTemplate;
