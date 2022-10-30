592b267f484c907d781581c9f96b0900f869305a
VagTsop <vatsop52@gmail.com>
2022-10-30 15:03:37 +0200

    add https ssl support

--- src/main/java/org/endofusion/endoserver/EndoServerApplication.java
+++ src/main/java/org/endofusion/endoserver/EndoServerApplication.java
@@ -21,7 +21,7 @@ public class EndoServerApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("https://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
